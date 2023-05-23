aafd5e983635b4e5e94ae38a152efd2f18321955
sterf-mac <stefhwin@gmail.com>
2023-05-23 18:46:12 -0400

    conversion of image to gray and show images

--- img_test.py
+++ img_test.py
@@ -9,5 +9,15 @@ pathname = "/images/"
 imgs = {}
 for filename in os.listdir(cwd + pathname):
     imgs[filename] = cv2.imread(cwd + pathname + filename)
+    if filename == "IMG_5255.jpg":
+        x = cv2.imread(cwd + pathname + filename)
 
-print(imgs.keys())
+
+print(imgs["IMG_5255.jpg"].shape)
+xg = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
+print(xg.shape)
+
+cv2.imshow("original", x)
+cv2.imshow("gray", xg)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
